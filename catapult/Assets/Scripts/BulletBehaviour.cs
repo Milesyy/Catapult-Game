8fdee6de1d61b7891c792dd2f0ca98385f7e904f
Milesyy <teacher@600900B202DE408.NSR.EDU.WIN>
2017-05-23 17:28:00 +1000

    Second Commit

---
+++ catapult/Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		transform.position += transform.forward;
+	}
+
+	//Make sure you have an attatched collider.
+	//Make sure that the collider is not a trigger.
+	//Make sure you have an attatched Rigidbody.
+	private void OnCollisionEnter (Collision collidedObject) {
+		print (collidedObject.gameObject.name);
+		if(collidedObject.gameObject.tag == "Enemy")  {
+			print ("EnemyHit");
+			Destroy (collidedObject.gameObject);
+		}
+	}
+}
---
+++ catapult/Assets/Scripts/shooting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour {
+
+	bool isPressed; 
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isPressed = Input.GetKey (KeyCode.Mouse0);
+		print (isPressed);
+	}
+
+	void FixedUpdate() {
+		if (isPressed == true) {
+			Instantiate (bulletPrefab,transform.position,transform.rotation);
+
+		}
+	}
+}
